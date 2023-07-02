bb60bd1081d8d61ebb6f2e0eb1c14cdaded461a5
Cassme1202 <95864723+Cassme1202@users.noreply.github.com>
2023-07-02 22:43:29 +0700

    test

---
+++ BookStore/RazorWeb/Pages/AdminPage/AdminManageBook.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorWeb.Pages.AdminPage
+{
+    public class AdminManageBookModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
