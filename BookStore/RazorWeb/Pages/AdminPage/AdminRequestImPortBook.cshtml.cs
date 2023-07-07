06d22dfdab0785cc3598e7c4b623ef1758eadbd2
Cassme1202 <95864723+Cassme1202@users.noreply.github.com>
2023-07-08 03:15:57 +0700

    update full admin page and staff and seller

---
+++ BookStore/RazorWeb/Pages/AdminPage/AdminRequestImPortBook.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorWeb.Pages.AdminPage
+{
+    public class AdminRequestImPortBookModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
