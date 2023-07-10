189fa234fd21750d1760ba5babf096d5f46ca244
Minhtda <113592001+Minhtda@users.noreply.github.com>
2023-07-10 13:27:47 +0700

    find book by category

--- BookStore/RazorWeb/Pages/AdminPage/AdminManageCategoryBook.cshtml.cs
+++ BookStore/RazorWeb/Pages/AdminPage/AdminManageCategoryBook.cshtml.cs
@@ -1,12 +1,34 @@
+using AutoMapper.Execution;
+using BookStoreAPI.Core.DTO;
+using BookStoreAPI.Core.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Service.Service.IService;
 
 namespace RazorWeb.Pages.AdminPage
 {
     public class AdminManageCategoryBookModel : PageModel
     {
+        IBookService _book;
+        ICategoryService _category;
+        public AdminManageCategoryBookModel(IBookService book, ICategoryService category)
+        {
+            _book = book;
+            _category = category;
+        }
+        [BindProperty]
+        public string search { get; set; }
+        public string nameCategory { get; set; }
+        [BindProperty(Name = "idc", SupportsGet = true)]
+        public int Id { get; set; }
+
+        public List<BookDTO> books { get; set; }
         public void OnGet()
         {
+            var listBook = _book.GetBookByCategory(Id);
+            books = listBook.Result.ToList();
+            var name = _category.GetCategoryById(Id).Result.Category_Name;
+            nameCategory = name;
         }
     }
 }
