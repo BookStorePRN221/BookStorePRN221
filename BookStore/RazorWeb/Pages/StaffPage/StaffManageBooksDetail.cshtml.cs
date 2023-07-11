1f7322c369984fcd84fdc826650aebd9c9d023b8
Cassme1202 <95864723+Cassme1202@users.noreply.github.com>
2023-07-11 10:00:13 +0700

    update all cshtml staff

---
+++ BookStore/RazorWeb/Pages/StaffPage/StaffManageBooksDetail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorWeb.Pages.StaffPage
+{
+    public class StaffManageBooksDetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
