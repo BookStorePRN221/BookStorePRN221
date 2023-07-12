cdd83bfd078215626c663aa8a805ba878954b3c5
Tuấn Vũ <113121915+John0209@users.noreply.github.com>
2023-07-12 16:24:05 +0700

    modal update

--- BookStore/RazorWeb/Pages/AdminPage/AdminManageBooksDetail.cshtml.cs
+++ BookStore/RazorWeb/Pages/AdminPage/AdminManageBooksDetail.cshtml.cs
@@ -10,19 +10,25 @@ namespace RazorWeb.Pages.AdminPage
     public class AdminManageBooksDetailModel : PageModel
     {
         private IBookService _book;
+        private ICategoryService _category;
         private IMapper _map;
-        public AdminManageBooksDetailModel(IBookService book,IMapper mapper)
+        public AdminManageBooksDetailModel(IBookService book,IMapper mapper, ICategoryService category)
         {
             _book = book;
             _map = mapper;
+            _category = category;
         }
         [BindProperty]
         public BookDetailDTO bookDetail { get; set; }
+        [BindProperty]
+        public List<Category> listCate { get; set; }
         [BindProperty(SupportsGet = true)]
         public Guid Book_Id { get; set; }
         public async Task OnGet()
         {
             bookDetail = await _book.GetBookById(Book_Id);
+            var list = await _category.GetAllCategory();
+            listCate=list.ToList();
         }
     }
 }
--- BookStore/RazorWeb/Pages/Login.cshtml.cs
+++ BookStore/RazorWeb/Pages/Login.cshtml.cs
@@ -7,6 +7,7 @@ namespace RazorWeb.Pages
     {
         public void OnGet()
         {
+            
         }
     }
 }
