45c478557efcdc1eab22d5f8f78919ca72ea0bdb
Tuấn Vũ <113121915+John0209@users.noreply.github.com>
2023-07-24 14:04:46 +0700

    update modal 2

--- BookStore/RazorWeb/Pages/StaffPage/StaffManageRequest.cshtml.cs
+++ BookStore/RazorWeb/Pages/StaffPage/StaffManageRequest.cshtml.cs
@@ -4,6 +4,7 @@ using BookStoreAPI.Core.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Service.Service.IService;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace RazorWeb.Pages.StaffPage
 {
@@ -13,6 +14,7 @@ namespace RazorWeb.Pages.StaffPage
         private ICategoryService _category;
         IMapper _map;
         private IBookService _book;
+        private bool m_update;
         public StaffManageRequestModel(IRequestService request, ICategoryService category, IMapper map, IBookService book)
         {
             _request = request;
@@ -20,6 +22,10 @@ namespace RazorWeb.Pages.StaffPage
             _map = map;
             _book = book;
         }
+        [BindProperty]
+        public bool is_Book_Create { get; set; }
+        [BindProperty]
+        public BookingRequest request { get; set; }
         [BindProperty]
         public List<BookingRequest> requestDTOs { get; set; }
         [BindProperty]
@@ -29,24 +35,54 @@ namespace RazorWeb.Pages.StaffPage
         public int m_Message { get; set; } = 0;
         [BindProperty]
         public BookDTO bookDTO { get; set; }
+        [BindProperty]
+        public List<Book> books { get; set; }
+
         public async Task OnGet()
         {
             var list = await _category.GetAllCategory();
             listCate = list.ToList();
             var listRequest = await _request.GetAllRequest();
             requestDTOs = listRequest.ToList();
+            var listBook = await _book.GetBook();
+            books = listBook.ToList();
         }
-        //public async Task<IActionResult> OnPost()
-        //{
-          
-        //    //await OnGet();
-        //    //if (_update)
-        //    //{
-        //    //    m_Message = 1;
-        //    //    return Page();
-        //    //}
-        //    //m_Message = 2;
-        //    //return Page();
-        //}
+        public async Task<IActionResult> OnPost(int number)
+        {
+            switch (number)
+            {
+                //create request
+                case 1:
+                    if (is_Book_Create)
+                    {
+                        request.Request_Amount = request.Request_Quantity * request.Request_Price;
+                        m_update = await _request.CreateRequest(request, is_Book_Create);
+                    }
+                    else
+                    {
+                        var books = await _book.GetBookById(request.Book_Id);
+                        request.Request_Amount = request.Request_Quantity * books.Book_Price;
+                        m_update = await _request.CreateRequest(request, is_Book_Create);
+                    }
+                    break;
+                //create book
+                case 2:
+                    var book = _map.Map<Book>(bookDTO);
+                    m_update = await _book.CreateBook(book, bookDTO.Image_URL, bookDTO.Request_Id);
+                    break;
+                case 3:
+                    m_update = await _request.UpdateRequestUnDone(request.Request_Id, request.Request_Note);
+                    break;
+            }
+
+            await OnGet();
+            if (m_update)
+            {
+                m_Message = 1;
+                return Page();
+            }
+            m_Message = 2;
+            return Page();
+        }
     }
 }
---
+++ BookStore/RazorWeb/wwwroot/js/booking.js
@@ -0,0 +1,19 @@
+﻿async function booking(id) {
+    const url1 = `https://book0209.azurewebsites.net/api/request/getRequestById?requestId=${id}`
+    const getTodo = async (url1) => {
+        return await fetch(url1);
+    };
+
+    const todo = await getTodo(url1);
+    const data = await todo.json();
+    console.log("data: ", data);
+    document.getElementById("name").value = data.request_Book_Name
+    document.getElementById("request").value = data.request_Id
+    document.getElementById("requestBook").value = data.request_Id
+    document.getElementById("url").value = data.request_Image_Url
+    document.getElementById("categoryId").value = data.category_Id
+    document.getElementById("quantity").value = data.request_Quantity
+    document.getElementById("price").value = data.request_Price
+   /* document.getElementById("name").innerHTML = data.request_Book_Name*/
+}
+
